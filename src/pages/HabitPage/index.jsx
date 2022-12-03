7ccf886db1f2a91c8ebb368cea6b29aa65badf1f
Lucas Saladini <lucassaladini@hotmail.com>
2022-12-03 11:59:25 -0300

    Create Habit and route structure

--- src/pages/HabitPage/index.jsx
+++ src/pages/HabitPage/index.jsx
@@ -2,8 +2,10 @@ import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native"
 
-export default function HabitPage() {
+export default function HabitPage({ route }) {
     const navigation = useNavigation()
+
+    const { create, habit } = route.params
 
     return (
         <View style={styles.container}>
@@ -14,7 +16,11 @@ export default function HabitPage() {
                             style={styles.arrowBack}/>
                     </TouchableOpacity>
                     <View style={styles.mainContent}>
-
+                        <Text style={styles.title}>Habit {"\n"} configuration</Text>
+                        <Text style={styles.inputText}>Area</Text>
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.area}>{habit?.habitArea}</Text>
+                        </View>
                     </View>
                 </View>
             </ScrollView>
@@ -42,5 +48,33 @@ const styles = StyleSheet.create({
     mainContent: {
         width: 250,
         alignSelf: "center"
+    },
+
+    title: {
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "white",
+        fontSize: 30
+    },
+
+    inputText: {
+        color: "white",
+        fontSize: 16,
+        marginTop: 35,
+        marginBottom: 10,
+        marginLeft: 5
+    },
+
+    inputContainer: {
+        borderWidth: 1,
+        borderColor: "#FFFFFF",
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 15
+    },
+
+    area: {
+        color: "#BBBBBB",
+        fontSize: 15
     }
 })
