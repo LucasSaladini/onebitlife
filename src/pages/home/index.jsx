614e1caeeade8f9ec38f5dcd0bf212664c1e0e2c
Lucas Saladini <lucassaladini@hotmail.com>
2022-12-03 10:34:25 -0300

    Button to create habits

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { useState, useEffect } from "react";
 import LifeStatus from "../../components/common/lifeStatus";
 import StatusBar from "../../components/home/StatusBar";
+import CreateHabit from "../../components/home/CreateHabit";
 
 export default function Home() {
     const navigation = useNavigation()
+
+    const [mindHabit, setMindHabit] = useState()
+    const [moneyHabit, setMoneyHabit] = useState()
+    const [bodyHabit, setBodyHabit] = useState()
+    const [moodHabit, setMoodHabit] = useState()
 
     function handleNavExplanation() {
         navigation.navigate("AppExplanation")
@@ -19,6 +26,7 @@ export default function Home() {
                     <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
                     <LifeStatus />
                     <StatusBar />
+                    <CreateHabit habitArea={"Mind"} borderColor={"#90B7F3"}/>
                 </View>
                 <Text style={styles.explanationText} onPress={() => {handleNavExplanation()}}>
                     See explanation again
