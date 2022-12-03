2b86326779b30d5a61a583d2f4b8b65f214db7e9
Lucas Saladini <lucassaladini@hotmail.com>
2022-12-02 21:46:30 -0300

    Structure of explanation page

---
+++ src/pages/AppExplanation/index.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
+
+
+import DefaultButton from "../../components/common/defaultButton"
+
+export default function AppExplanation() {
+    function handleSetShowHome() {
+        console.log("Button clicked")
+    }
+
+    return (
+        <View styles={styles.container}>
+            <ScrollView>
+                <View style={{ alignItems: "center" }}>
+                    <Text style={styles.title}>First, let me {"\n"} explain something...</Text>
+                    <Text style={styles.descriptionCTA}>Ready to level up on life?</Text>
+                    <Text style={styles.description}>On next screen you will choose {"\n"}
+                        your four habits individually.
+                    </Text>
+                    <DefaultButton buttonText={"Continue"} handlePress={handleSetShowHome}
+                        width={250} height={50} />
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#rgba (21, 21, 21, 0.98",
+    },
+
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center",
+        marginVertical: 40
+    },
+
+    descriptionCTA: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
+        marginTop: 20,
+        marginBottom: 10
+    },
+
+    description: {
+        color: "white",
+        textAlign: "center",
+        marginBottom: 30,
+    }
+})
