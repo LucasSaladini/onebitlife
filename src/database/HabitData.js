756884146c98833cd67c88f9b2cff74f793a22c7
Lucas Saladini <lucassaladini@hotmail.com>
2022-12-03 15:38:44 -0300

    Inserted SelectHabit, Dropdown list and Database

---
+++ src/database/HabitData.js
@@ -0,0 +1,101 @@
+const dataMind = [
+    {
+        key: "Study some subject for 30 minutes",
+        value: "Study some subject for 30 minutes"
+    },
+    {
+        key: "Study some subject for 60 minutes",
+        value: "Study some subject for 60 minutes"
+    },
+    {
+        key: "Read a book for 15 minutes",
+        value: "Read a book for 15 minutes"
+    },
+    {
+        key: "Read a book for 30 minutes",
+        value: "Read a book for 30 minutes"
+    },
+    {
+        key: "Work on a personal project for 30 minutes",
+        value: "Work on a personal project for 30 minutes"
+    },
+    {
+        key: "Work on a personal project for 60 minutes",
+        value: "Work on a personal project for 60 minutes"
+    }
+]
+
+const dataMoney = [
+    {
+        key: "Save 10% of salary",
+        value: "Save 10% of salary"
+    },
+    {
+        key: "Save 20% of salary",
+        value: "Save 20%  of salary"
+    },
+    {
+        key: "Send 10 freelance proposal",
+        value: "Send 10 freelance proposal"
+    },
+    {
+        key: "Send 20 freelance proposal",
+        value: "Send 20 freelance proposal"
+    },
+    {
+        key: "Apply for 10 from companies that pay better than the current one",
+        value: "Apply for 10 from companies that pay better than the current one"
+    },
+    {
+        key: "Apply for 20 from companies that pay better than the current one",
+        value: "Apply for 20 from companies that pay better than the current one"
+    }
+]
+
+const dataBody = [
+    {
+        key: "Workout on gym for 45 minutes",
+        value: "Workout on gym for 45 minutes"
+    },
+    {
+        key: "Practice yoga for 15 minutes",
+        value: "Practice yoga for 15 minutes"
+    },
+    {
+        key: "Daily walk for 15 minutes",
+        value: "Daily walk for 15 minutes"
+    },
+    {
+        key: "Daily walk for 30 minutes",
+        value: "Daily walk for 30 minutes"
+    },
+    {
+        key: "Meditate for 30 minutes",
+        value: "Meditate for 30 minutes"
+    }
+]
+
+const dataMood = [
+    {
+        key: "Play video-game for 30 minutes",
+        value: "Play video-game for 30 minutes"
+    },
+    {
+        key: "Play video-game for 60 minutes",
+        value: "Play video-game for 60 minutes"
+    },
+    {
+        key: "Watch a series or movie for 30 minutes",
+        value: "Watch a series or movie for 30 minutes"
+    },
+    {
+        key: "Watch a series or movie for 60 minutes",
+        value: "Watch a series or movie for 60 minutes"
+    },
+    {
+        key: "Hangout with friends",
+        value: "Hangout with friends"
+    }
+]
+
+export default { dataMind, dataBody, dataMoney, dataMood }
--- src/pages/HabitPage/index.jsx
+++ src/pages/HabitPage/index.jsx
@@ -1,9 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useState, setHabitInput } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native"
+import SelectHabit from "../../components/HabitPage/SelectHabit";
 
 export default function HabitPage({ route }) {
     const navigation = useNavigation()
+
+    const [habitInput, setHabitInput] = useState()
 
     const { create, habit } = route.params
 
@@ -21,6 +24,8 @@ export default function HabitPage({ route }) {
                         <View style={styles.inputContainer}>
                             <Text style={styles.area}>{habit?.habitArea}</Text>
                         </View>
+                        <Text style={styles.inputText}>Habit</Text>
+                        <SelectHabit habit={habit} habitInput={setHabitInput} />
                     </View>
                 </View>
             </ScrollView>
